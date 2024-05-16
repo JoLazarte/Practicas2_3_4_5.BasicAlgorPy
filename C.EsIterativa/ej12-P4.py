3d762bcb7b30f7d7f3f4440a86c5319a7fe2c1c4
Joanna-Lazarte <jlazartelagos@gmai.com>
2024-05-16 00:40:24 -0300

    fibCorrected

--- C.EsIterativa/ej12-P4.py
+++ C.EsIterativa/ej12-P4.py
@@ -7,6 +7,7 @@ Realizar un programa que lea N e imprima los N primeros términos de la sucesió
 # EPS+CE
 
 #Entrada: Se lee N
+
 n=int(input("Ingrese nro de termino: "))
 
 #PROC******************************************************************************
@@ -15,31 +16,37 @@ aa=0  #primer termino  fib(0)
 a=1   #segundo termino  fib(1)
 
 if n > 1:  #siempre se va a imprimir el primer y segundo termino iniciales.
-    print (aa)   #primer término
-    print(a)     #segundo término
+    print ("el primer término en la sucesión fibonacci es: ", aa)   #primer término
+    print("el segundo término en la sucesión fibonacci es: ", a)     #segundo término
     #inicializar
-    s=aa+a #el resultado siempre va a ser 1
-    for c in range(3,n+1): #La VUELTA SE INICIA 3, PORQUE FIB(2) PUEDE CALCULARSE AL INICIO DEL "IF" (SI n ES 2, EL BUCLE NO SE INICIA, porque empieza en 3)
+    fb=0 #inicializo en 0 la suma fib de n
+    for c in range(2,n+1): #La VUELTA SE INICIA 2, poque ya tengo FIB(0) y FIB(1), TERMINA en n+1, pra que ya no se calcule el término siguiente al ingresado.
         #Subproceso:
-        fb=a+aa  # suma de los dos terminos anteriores. PRIMERA VUELTA fb=1; SEGUNDA (SI n > 3), fb=2; TERCERA, fb=3
+        fb=a+aa  # suma de los dos terminos anteriores. PRIMERA VUELTA fb(2)=1; SEGUNDA (SI n > 3), fb(3)=2; TERCERA, fb(4)=3
         print("Por el momento, la suma es : ", fb)
-        #Defino los nuevos 2 ultimos terminos:
-        aa=a    # lo que valia a ahora se guarda en aa, aa. 1ra VUELTA aa=1; SEGUNDA, aa=1; TERCERA, aa=2
-        a=fb    # nuevo valor de a = aa anterior + a anterior. 1ra VUELTA a=1; SEGUNDA, a=2; TERCERA, a=3
-        s+=fb #suma total actualizada: s anterior + fb actual. 1ra VUELTA: s=2; SEGUNDA: s=4 (s anterior = 2 + fb actual =2); TERCERA: s=7 (sAnterior=4+fbActual=3)
-else: #si n es 0 o 1, la suma siempre será 0 y se imprime el unico valor anterior: 0
-     s=0
-     print (aa)
+        #Defino los nuevos 2 ultimos terminos, para la 2da y siguientes:
+        aa=a # lo que valia a ahora se guarda en aa, aa. 1ra VUELTA aa=1; SEGUNDA, aa=1; TERCERA, aa=2
+        a=fb # nuevo valor de a = aa anterior + a anterior. 1ra VUELTA a=1; SEGUNDA, a=2; TERCERA, a=3
+    
+else: #si n es 0 o 1, la suma siempre será 0 o 1
+    fb= aa + n
+    print ("el primer término en la sucesión fibonacci es: ", aa)  #primer término
 #FIN PROC *************************************************************************    
 #Salida:    
-print("La suma es ",s)
+print(f"La suma para el nro término {n} es: ",fb)
 
 #*****OPCIÓN MÁS SIMPLE******************************************************************************
-a1=1
-a2=1
-N=int(input("Ingrese nro :"))
-f=1
-for i in range(2,N):
-    f=a1+a2
-    a1,a2=a2,f
+a1=1 #f(1)
+a2=1 #f(2)
+N=int(input("Ingrese nro: "))
+f=-1
+if N == 0:
+    f=0
+else:
+    f=1 #cuando N=1 o N=2, f=0
+    #En la primera vuelta, con N=2, se mantiene l valor de f=1, empieza a cambiar en la siguiente vuelta, despues de reasignar los valores de a1 y a2
+    for i in range(2,N): #dara una vuelta menos a N, porque la suma f comienza en f(3)=f(1)+f(2)=2
+        f=a1+a2
+        a1,a2=a2,f
+        
 print(f)
--- C.EsIterativa/ej12B-P4.py
+++ C.EsIterativa/ej12B-P4.py
@@ -1,13 +1,15 @@
 def Fibonaci(n):
-    a1=0
-    a2=1
-    f=0
-    for i in range(2,n+1):
-        f=a1+a2
-        a1,a2=a2,f
+    a1=0 #f(0)
+    a2=1 #f(1)
+    f=0 #suma de los últimos términos para f(0)
+    if N==1:
+        f=1 #suma de los últimos términos para f(1)
+    else:
+        for i in range(2,n+1):#el bucle comienza en f(2), con f=1 y se detendrá en f(N+1), que no será incliudo.
+            f=a1+a2 #suma de los últimos términos para f(N), desde f(2) hasta f(N)
+            a1,a2=a2,f
     return f
     
-
 N=int(input("Ingrese nro: "))
 
 print(Fibonaci(N))
