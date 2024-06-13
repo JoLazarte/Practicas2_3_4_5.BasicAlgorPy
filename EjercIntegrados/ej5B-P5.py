e30832e0f7604da61c21043e1974460d5002a225
Joanna-Lazarte <jlazartelagos@gmai.com>
2024-06-13 01:01:01 -0300

    corrections5bp5

--- EjercIntegrados/ej5B-P5.py
+++ EjercIntegrados/ej5B-P5.py
@@ -33,81 +33,105 @@ Pendientes: Considerar que meses tienen 30 días y cuales solo 30, además de cu
 #----------------------------------------------------------------------------------------------
 # FUNCIONES
 #----------------------------------------------------------------------------------------------
+def bisiesto(yearBis):
+    bisiesto=False
+    if yearBis % 4 ==0:
+        if yearBis % 100 ==0:
+            if yearBis % 400==0:
+                bisiesto=True
+        else:
+            bisiesto=True
+
+    return bisiesto
+
+def nonEvenMonth(currentMonth):
+    evenMonth=False
+    if(currentMonth == 1 or (currentMonth > 2 and currentMonth < 13)): 
+        #meses de 31 días:
+        if((currentMonth > 7 and currentMonth % 2==0) or (currentMonth <= 7 and currentMonth % 2==1)):
+            evenMonth=True  
+    return evenMonth
+           
+
 def validDate(day, month, year):
-    #desarrollo
+   
     fecha=False
     #rango de años válidos:
     if(year>1580 and year<2030):
         #mes de febrero:
         if(month == 2):
-            #¿es bisiesto?
-            if((((year % 4 == 0) and not(year % 100 == 0)) or (year % 400 == 0)) and (day > 0 and day<30)):
-                fecha=True
-            #no es bisiesto:
-            elif(day > 0 and day <29):
-                fecha=True
+            #Bisiesto
+            if((bisiesto(year)==True and (day>0 and day<30)) or (bisiesto(year)==False and (day>0 and day<29))):
+                fecha=True        
         #resto de los meses:
-        elif(month == 1 or (month > 2 and month < 13)):
-            #meses de 31 días:
-            if((day>0 and day<32)and(month !=2 and month != 4 and month != 6 and month != 9 and  month != 11)):
-                fecha=True           
-            #meses de 30 días:
-            elif((day>0 and day<31)and(month == 4 or month == 6 or month == 9 or  month == 11)):
-                fecha=True           
-   
-    return  fecha
+        elif((nonEvenMonth(month)==True and (day>0 and day<32)) or (nonEvenMonth(month)==False and (day>0 and day<31))):
+            fecha=True        
+                        
+    return fecha
 
 def diaSiguiente(day, month, year):
     
     valid=validDate(day, month, year)
     fecha=[]
     if(valid==True):
-    
-        if((day>0 and day<31)and(month !=2 and month != 4 and month != 6 and month != 9 and  month != 11)):
-            day=day+1
-            fecha=[day, month, year]
-            print("La fecha del día siguiente es: ", fecha)
-        else:
-            if((day>0 and day<30)and(month == 4 or month == 6 or month == 9 or  month == 11)):
-                day=day+1
-                fecha=[day, month, year]
-                print("La fecha del día siguiente es: ", fecha)
-            else:
-                if((day>0 and day<28)and(month == 2)):
+
+        if(month==2):
+            if(bisiesto(year)==False):
+                if(day==28):
+                    day=1
+                    month=3
+                    fecha=[day, month, year]
+                    print("La fecha del día siguiente es: ", fecha)    
+                elif(day<28):
                     day=day+1
                     fecha=[day, month, year]
                     print("La fecha del día siguiente es: ", fecha)
-                else:
-                    if((day>0 and day<29)and(month == 2) and (((year % 4 == 0) and not(year % 100 == 0)) or (year % 400 == 0))):
+            elif(bisiesto(year)==True):
+                if(day==29):
+                    day=1
+                    month=3
+                    fecha=[day, month, year]
+                    print("La fecha del día siguiente es: ", fecha)    
+
+                elif(day<29):
+                    day=day+1
+                    fecha=[day, month, year]
+                    print("La fecha del día siguiente es: ", fecha)
+
+        elif(month!=2):
+            if(month!=12):
+                if(nonEvenMonth(month)==True):
+                    if(day==31):  
+                        day=1
+                        month=month+1
+                        fecha=[day, month, year]
+                        print("La fecha del día siguiente es: ", fecha)
+                    elif(day<31):
                         day=day+1
                         fecha=[day, month, year]
                         print("La fecha del día siguiente es: ", fecha)
-                    else:
-                        if((day==31 or day == 30) and (month != 12)):
-                            day=1
-                            month=month+1
-                            fecha=[day, month, year]
-                            print("La fecha del día siguiente es: ", fecha)
-                        else:
-                            if(day==31 and month == 12):
-                                day=1
-                                month=1
-                                year=year+1
-                                fecha=[day, month, year]
-                                print("La fecha del día siguiente es: ", fecha)
-                            else:
-                                if((day==28) and month == 2):
-                                    day=1
-                                    month=3
-                                    fecha=[day, month, year]
-                                    print("La fecha del día siguiente es: ", fecha)
-                                else:
-                                    if((day==29) and (month == 2) and (((year % 4 == 0) and not(year % 100 == 0)) or (year % 400 == 0))):
-                                        day=1
-                                        month=3
-                                        fecha=[day, month, year]
-                                        print("La fecha del día siguiente es: ", fecha)  
-        
+                if(nonEvenMonth(month)==False):
+                    if(day==30):  
+                        day=1
+                        month=month+1
+                        fecha=[day, month, year]
+                        print("La fecha del día siguiente es: ", fecha)
+                    elif(day<30):
+                        day=day+1
+                        fecha=[day, month, year]
+                        print("La fecha del día siguiente es: ", fecha)
+            elif(month == 12):
+                if(day==31):
+                    day=1
+                    month=1
+                    year=year+1
+                    fecha=[day, month, year]
+                    print("La fecha del día siguiente es: ", fecha)
+                elif(day<31):
+                    day=day+1
+                    fecha=[day, month, year]
+                    print("La fecha del día siguiente es: ", fecha)
+                 
     else:
         print("Fecha inválida")
         
